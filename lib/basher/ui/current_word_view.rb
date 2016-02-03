2b1a59a7282ed8e154cc7015c6e07fb0a0259aa7
shuriu <stefan.rotariu@gmail.com>
2016-02-03 18:36:54 +0200

    Current word is ascii as well

--- lib/basher/ui/current_word_view.rb
+++ lib/basher/ui/current_word_view.rb
@@ -4,12 +4,22 @@ module Basher
       attr_accessor :game
 
       def self.lines
-        6
+        9
       end
 
+      # TODO: Figure out how to make the text center line not move
+      # as fewer letters remain in the word.
       def setup
         clear
-        puts game.word.remaining, h: :center, v: :center
+        text = game.word.remaining.ascii(font: 'roman')
+        # Height ranges from 5 to 9
+        height = text.lines.size
+        offset = (9 - height) / 2
+        move_cursor line: offset
+
+        text.lines.each_with_index do |line, index|
+          puts line, h: :center
+        end
       end
     end
   end

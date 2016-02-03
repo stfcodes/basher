82a46f36e64a3d296ce57bdeb12c53a01f894059
shuriu <stefan.rotariu@gmail.com>
2016-02-03 18:35:41 +0200

    Simplify the ascii refinement

--- lib/basher/refinements/string.rb
+++ lib/basher/refinements/string.rb
@@ -1,23 +1,15 @@
 require 'artii'
+require 'pry'
+require 'binding_of_caller'
 
 module Basher
   module StringRefinements
     refine String do
       def ascii(font: 'broadway')
         asciifier = Artii::Base.new(font: font)
-        art = asciifier.asciify(self)
-
-        # First remove the newlines
-        lines = art.lines.map { |l| l.gsub("\n", '') }
-
-        # Some Artii fonts have empty lines before the
-        #  actual character lines, so remove them.
-        first_non_empty_line = lines.index { |l| !l.strip.empty? }
-        lines = lines[first_non_empty_line..-1]
-
-        # Now remove the last empty lines, and return a string
-        first_empty_line = lines.index { |l| l.strip.empty? }
-        lines[0..(first_empty_line || -1)].join("\n")
+        art       = asciifier.asciify(self)
+        lines     = art.lines.map { |l| l.gsub("\n", '') }
+        lines.select { |l| !l.strip.empty? }.join("\n")
       end
     end
   end
